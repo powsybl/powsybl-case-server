1d969775a7ba9ebca9b1fd66bbbfa1d9fb48e1ca
Rehili Ghazwa <ghazwarhili@gmail.com>
2023-11-06 17:25:33 +0100

    unify testcontainer config
    Signed-off-by: Rehili Ghazwa <ghazwarhili@gmail.com>

--- src/test/java/com/powsybl/caseserver/datasource/util/S3CaseDataSourceControllerTest.java
+++ src/test/java/com/powsybl/caseserver/datasource/util/S3CaseDataSourceControllerTest.java
@@ -13,13 +13,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.Testcontainers;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
@@ -28,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
-import java.time.Duration;
 import java.util.*;
 
 /**
@@ -40,42 +34,6 @@ import java.util.*;
 @TestPropertySource(properties = {"storage.type=S3"})
 @ContextConfigurationWithTestChannel
 public class S3CaseDataSourceControllerTest extends AbstractCaseDataSourceControllerTest {
-
-    // TODO MOVE THIS TO SEPARATE CLASS
-    private static final String MINIO_DOCKER_IMAGE_NAME = "minio/minio";
-    private static final String BUCKET_NAME = "bucket-gridsuite";
-    // Just a fixed version, latest at the time of writing this
-    private static final String MINIO_DOCKER_IMAGE_VERSION = "RELEASE.2023-09-27T15-22-50Z";
-    private static GenericContainer minioContainer;
-    private static final int MINIO_PORT = 9000;
-
-    // can't use a bean because we need it before spring aws autoconfiguration TODO
-    static {
-        minioContainer = new GenericContainer(
-                String.format("%s:%s", MINIO_DOCKER_IMAGE_NAME, MINIO_DOCKER_IMAGE_VERSION))
-                // .withClasspathResourceMapping("/", "/data/", BindMode.READ_WRITE)
-                .withCommand("server /data").withExposedPorts(MINIO_PORT).waitingFor(new HttpWaitStrategy()
-                        .forPath("/minio/health/ready").forPort(MINIO_PORT).withStartupTimeout(Duration.ofSeconds(10)));
-        minioContainer.start();
-        try {
-            minioContainer.execInContainer("mkdir", "/data/" + BUCKET_NAME);
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
-
-        // can't use System.setProperty because spring cloud aws gets the url early
-        // System.setProperty("spring.cloud.aws.s3.endpoint", minioContainerUrl);
-    }
-
-    @DynamicPropertySource
-    static void registerPgProperties(DynamicPropertyRegistry registry) {
-        Integer mappedPort = minioContainer.getFirstMappedPort();
-        Testcontainers.exposeHostPorts(mappedPort);
-        String minioContainerUrl = String.format("http://172.17.0.1:%s", mappedPort);
-
-        registry.add("spring.cloud.aws.endpoint", () -> minioContainerUrl);
-    }
-    // END TODO
 
     @Autowired
     private S3Client s3Client;
