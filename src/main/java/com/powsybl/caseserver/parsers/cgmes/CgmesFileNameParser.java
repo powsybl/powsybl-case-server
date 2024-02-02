1e4c45efbe6ec686f653b5338f543923ea101898
Antoine Bouhours <123802855+antoinebhs@users.noreply.github.com>
2024-02-02 10:48:30 +0100

    powsybl-ws-dependencies 2.7.0 (#23)
    Signed-off-by: BOUHOURS Antoine <antoine.bouhours@rte-france.com>

--- src/main/java/com/powsybl/caseserver/parsers/cgmes/CgmesFileNameParser.java
+++ src/main/java/com/powsybl/caseserver/parsers/cgmes/CgmesFileNameParser.java
@@ -8,16 +8,16 @@ package com.powsybl.caseserver.parsers.cgmes;
 
 import com.google.auto.service.AutoService;
 import com.powsybl.caseserver.parsers.FileNameParser;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A utility class to work with Entsoe file name parsers.
+ * A utility class to work with Cgmes file name parsers.
  *
  * @author Franck Lecuyer <franck.lecuyer at rte-france.com>
  */
@@ -27,9 +27,11 @@ public class CgmesFileNameParser implements FileNameParser {
     public static final Pattern FILE_NAME_REGEX = Pattern.compile("(\\d{8}[T]\\d{4}[Z])[_](\\w{2})[_](.+)[_](\\d{3}).*");
     public static final String DATE_FORMAT = "yyyyMMdd'T'HHmm'Z'";
 
-    public static DateTime parseDateTime(String dateStr) {
-        DateTimeFormatter df = DateTimeFormat.forPattern(DATE_FORMAT);
-        return df.withZoneUTC().parseDateTime(dateStr);
+    public static ZonedDateTime parseDateTime(String dateStr) {
+        DateTimeFormatter formatter = DateTimeFormatter
+                .ofPattern(DATE_FORMAT)
+                .withZone(ZoneOffset.UTC);
+        return ZonedDateTime.parse(dateStr, formatter);
     }
 
     @Override
@@ -43,7 +45,7 @@ public class CgmesFileNameParser implements FileNameParser {
         if (!m.matches()) {
             return Optional.empty();
         }
-        DateTime date = parseDateTime(m.group(1));
+        ZonedDateTime date = parseDateTime(m.group(1));
         String businessProcess = m.group(2).substring(0, 2);
         String tso = m.group(3);
         int version = Integer.parseInt(m.group(4).substring(0, 3));
