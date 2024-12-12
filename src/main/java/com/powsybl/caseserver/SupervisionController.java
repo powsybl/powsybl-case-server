27f7233c037e08b2d1fd6041ce96370c1b80d759
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2024-12-12 09:21:13 +0100

    feat: upgrade ws-commons to 1.18.0
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/com/powsybl/caseserver/SupervisionController.java
+++ src/main/java/com/powsybl/caseserver/SupervisionController.java
@@ -17,7 +17,6 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
