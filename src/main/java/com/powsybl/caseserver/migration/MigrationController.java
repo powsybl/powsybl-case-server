d11a72ef9d294d34014484d7cd756a9ee7091e1d
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-06-05 16:18:39 +0200

    checkstyle
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/main/java/com/powsybl/caseserver/migration/MigrationController.java
+++ src/main/java/com/powsybl/caseserver/migration/MigrationController.java
@@ -16,7 +16,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
-import org.apache.commons.compress.utils.FileNameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +27,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.nio.file.Paths;
 import java.util.*;
 
 /**
