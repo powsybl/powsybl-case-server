41675e4f5aa6bbda05fb78bb8aed48a1ee260870
basseche <bassel.el-cheikh_externe@rte-france.com>
2025-06-26 15:42:18 +0200

    checkstyle
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/test/java/com/powsybl/caseserver/service/CaseServiceTest.java
+++ src/test/java/com/powsybl/caseserver/service/CaseServiceTest.java
@@ -23,7 +23,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.UUID;
 
