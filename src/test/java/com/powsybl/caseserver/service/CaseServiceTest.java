b7f1b2454bda652fed2e402fe818553c20baf86c
Etienne <etiennehomer@gmai.com>
2025-06-27 22:00:05 +0200

    clean
    Signed-off-by: Etienne <etiennehomer@gmai.com>

--- src/test/java/com/powsybl/caseserver/service/CaseServiceTest.java
+++ src/test/java/com/powsybl/caseserver/service/CaseServiceTest.java
@@ -25,7 +25,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
