6695aa98a25d383849e858ea5953a2f270805e10
Etienne Homer <etiennehomer@gmail.com>
2025-07-03 14:04:15 +0200

    clean
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/test/java/com/powsybl/caseserver/service/CaseServiceTest.java
+++ src/test/java/com/powsybl/caseserver/service/CaseServiceTest.java
@@ -23,9 +23,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
