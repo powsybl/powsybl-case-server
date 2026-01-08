266557317d72eb30656aadd45e33f94e72275f84
basseche <bassel.el-cheikh_externe@rte-france.com>
2026-01-08 10:48:33 +0100

    fix checkstyle issus
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/main/java/com/powsybl/caseserver/CaseController.java
+++ src/main/java/com/powsybl/caseserver/CaseController.java
@@ -28,7 +28,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
